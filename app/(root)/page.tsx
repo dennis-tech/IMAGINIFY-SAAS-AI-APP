0000f671ccdbe0532d191a2b10bdac75afe18524
dennis-tech <hedenniskibe@gmail.com>
2024-03-06 23:33:25 +0300

    created sidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,11 +2,7 @@ import { UserButton } from '@clerk/nextjs'
 
 const Home = () => {
   return (
-    <div>
-      <p>Home</p>
-
-      <UserButton afterSignOutUrl='/' />
-    </div>
+    <div></div>
   )
 }
 
