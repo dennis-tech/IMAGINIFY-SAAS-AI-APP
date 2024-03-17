abf3e37cdaee6dae891bab0b02bdc926fc57b55a
dennis-tech <hedenniskibe@gmail.com>
2024-03-17 16:17:08 +0300

    Updated import on credits page

--- app/(root)/credits/page.tsx
+++ app/(root)/credits/page.tsx
@@ -6,7 +6,8 @@ import Header from "@/components/shared/Header";
 import { Button } from "@/components/ui/button";
 import { plans } from "@/constants";
 import { getUserById } from "@/lib/actions/user.actions";
-import Checkout from "@/components/shared/Checkout";
+import Checkout from "@/components/shared/CheckOut";
+
 
 const Credits = async () => {
   const { userId } = auth();
